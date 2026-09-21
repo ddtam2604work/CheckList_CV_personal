0c62afc3ffc5beaabf27ead5151d820be06c923d
ddtam2604work <ddtam2604.work@gmail.com>
2026-09-21 11:53:10 +0700

    fix: universal GitHub Pages support with relative paths, root assets and nojekyll

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CheckList_CV_personal/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
